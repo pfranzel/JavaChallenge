ff111d7822f416024af7bfa6ba5348c66a27bb8a
pfranzel <80775775+pfranzel@users.noreply.github.com>
2021-06-02 17:14:20 +0200

    Just format change...

--- src/main/java/de/telekom/sea/Main.java
+++ src/main/java/de/telekom/sea/Main.java
@@ -7,9 +7,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println("Part1 solution:");
 		CodeSnippletPart1 csp1 = new CodeSnippletPart1();
 		csp1.snippletPart1();
 		
+		System.out.println();
+		System.out.println("Part2 solution:");
 		CodeSnippletPart2 csp2 = new CodeSnippletPart2();
 		csp2.snippletPart2();
 		
