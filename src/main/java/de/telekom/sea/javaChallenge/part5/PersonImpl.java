2cbac46668577d0d8dcb62638c9247bbac0ea678
pfranzel <80775775+pfranzel@users.noreply.github.com>
2021-06-03 20:32:38 +0200

    Add Part 5 / 1. Version

--- src/main/java/de/telekom/sea/javaChallenge/part5/PersonImpl.java
+++ src/main/java/de/telekom/sea/javaChallenge/part5/PersonImpl.java
@@ -1,5 +1,24 @@
 package de.telekom.sea.javaChallenge.part5;
 
-public class PersonImpl {
+public class PersonImpl implements Person {
 
+	private String firstname;
+	private String lastname;
+	
+	public PersonImpl() {
+		
+	}
+	
+	public PersonImpl(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;		
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
 }
